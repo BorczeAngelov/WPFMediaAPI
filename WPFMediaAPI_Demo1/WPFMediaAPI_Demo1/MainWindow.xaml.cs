087f0392333c8dcf86c6d6b97151fa19c44475ab
Borcze Angelov <borcze.angelov@yahoo.com>
2020-10-29 22:13:37 +0100

    second commit

---
+++ WPFMediaAPI_Demo1/WPFMediaAPI_Demo1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace WPFMediaAPI_Demo1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMediaElementFile(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Media files (*.wmv)|*.wmv|All Files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog()== true)
+            {
+                var fullFileName = openFileDialog.FileName;
+                var mediaFileUri = new Uri(fullFileName);
+                MediaElement.Source = mediaFileUri;
+                MediaElement.Play();
+            }
+        }
+
+        //private void OpenMediaPlayerElementFile(object sender, RoutedEventArgs e)
+        //{
+        //    var openFileDialog = new OpenFileDialog();
+        //    openFileDialog.Filter = "Media files (*.wmv)|*.wmv|All Files (*.*)|*.*";
+
+        //    if (openFileDialog.ShowDialog() == true)
+        //    {
+        //        var fullFileName = openFileDialog.FileName;
+        //        var mediaFileUri = new Uri(fullFileName);
+        //        MediaPlayerElement.Source = fullFileName;
+        //        MediaElement.Play();
+        //    }
+        //}
+    }
+}
